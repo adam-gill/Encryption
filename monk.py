72523770798b21c6d6354f1acf508c97eef290b8
adam-gill <adam.douglas.gill@gmail.com>
2025-02-20 10:13:51 -0500

    new

--- monk.py
+++ monk.py
@@ -6,6 +6,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import Fernet
 
+# test
 
 if not os.path.exists('out.txt'):
     file = open('out.txt', 'wb')
